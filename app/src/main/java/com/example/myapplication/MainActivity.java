5bdcfe02bcfa8f884573868761eabcfd3460253f
OscarBenites <oscar.benites_825@hotmail.com>
2022-04-26 21:57:56 -0500

    Add-Service V1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,12 +24,15 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner spnCategoria;
+    Spinner spnCategoria,spnTipo;
+
     ArrayAdapter<String> adapter;
     ArrayList<String> libros = new ArrayList<String>();
     List<Libro> lstLibros = new ArrayList<Libro>();
+
     Button btnRegistrar;
-    EditText txtInformacion;
+    EditText txtTitulo, txtAnio, txtSerie, txtFechaCreacion, txtFechaRegistro, txtEstado;
+
     LibroService libroService;
 
     @Override
@@ -36,14 +40,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spnCategoria = findViewById(R.id.spnCategoria);
+        libroService = Connection.getConnecion().create(LibroService.class);
+
+        spnCategoria = findViewById(R.id.spnECategoria);
+        spnTipo = findViewById(R.id.spnETipo);
+        txtTitulo = findViewById(R.id.txtEditTitulo);
+        txtAnio = findViewById(R.id.txtEditAnio);
+        txtSerie = findViewById(R.id.txtEditSerie);
+        txtFechaCreacion = findViewById(R.id.txtEditFechaCreacion);
+        txtFechaRegistro = findViewById(R.id.txtEditFechaRegistro);
+        txtEstado = findViewById(R.id.txtEditEstado);
+        btnRegistrar = findViewById(R.id.btnBRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tit = txtTitulo.getText().toString();
+                String ani = txtAnio.getText().toString();
+                String cat = spnCategoria.getSelectedItem().toString();
+                String ser = txtSerie.getText().toString();
+                String tip = spnTipo.getSelectedItem().toString();
+                String fcr = txtFechaCreacion.getText().toString();
+                String fre = txtFechaRegistro.getText().toString();
+                String est = txtEstado.getText().toString();
+
+
+                Libro obj = new Libro();
+                obj.setTitulo(tit);
+                obj.setAnio(ani);
+                obj.setCategoria(cat);
+                obj.setSerie(ser);
+                obj.setTipo(tip);
+                obj.setFechacreacion(fcr);
+                obj.setFechaRegistro(fre);
+                obj.setEstado(Integer.parseInt(est));
+
+                registar(obj);
+            }
+        });
+
         adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, libros);
         spnCategoria.setAdapter(adapter);
+        spnTipo.setAdapter(adapter);
 
-        btnRegistrar = findViewById(R.id.btnRegistrar);
-        txtInformacion = findViewById(R.id.txtInformacion);
-
-        libroService = Connection.getConnecion().create(LibroService.class);
+        btnRegistrar = findViewById(R.id.btnBRegistrar);
 
         cargarDatos();
     }
@@ -53,10 +93,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<Libro>> call, Response<List<Libro>> response) {
                 if(response.isSuccessful()){
-                    mensajeToast("Acceso exitoso al servicio REST");
                     lstLibros = response.body();
                     for (Libro obj : lstLibros){
-                        libros.add(obj.getTitulo());
+                        libros.add(obj.getCategoria());
                     }
                     adapter.notifyDataSetChanged();
                 }
@@ -67,6 +106,25 @@ public class MainActivity extends AppCompatActivity {
                     mensajeToast("Error al conectarse al servicio REST");
             }
         });
+    }
+    public void registar(Libro obj){
+    Call<Libro> call = libroService.insertarLibro(obj);
+    call.enqueue(new Callback<Libro>() {
+        @Override
+        public void onResponse(Call<Libro> call, Response<Libro> response) {
+            if(response.isSuccessful()){
+                Libro objRetorno = response.body();
+                mensajeAlert("Libro registrado correctamente");
+            } else {
+                mensajeAlert("Error al registrar libro");
+            }
+        }
+
+        @Override
+        public void onFailure(Call<Libro> call, Throwable t) {
+            mensajeToast(t.getMessage());
+        }
+    });
     }
     public void mensajeAlert(String msg){
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
---
+++ app/src/main/java/com/example/myapplication/util/FunctionUtil.java
@@ -0,0 +1,6 @@
+package com.example.myapplication.util;
+
+public class FunctionUtil {
+    public static int ESTADO_ACTIVO = 1;
+    public static int ESTADO_INACTIVO = 0;
+}
